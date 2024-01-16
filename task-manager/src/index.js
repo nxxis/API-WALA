5ad25a41e21fec59011e98d84491722a89d6caf6
Sudip Sharma <sudip.sharma@ebpearls.com>
2024-01-16 17:25:08 +0545

    authentication implemented on routes with jwt

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 require('./db/mongoose');
 
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   console.log(req.method, req.path);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res
+//     .status(503)
+//     .send('Server is under maintainance, please try again after some time');
+// });
+
 app.use(express.json()); /// parses incoming JSON to an Object
-app.use(userRouter)
-app.use(taskRouter)
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log('server is running on port ' + port);
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -9,7 +11,7 @@ const User = mongoose.model('User', {
   },
   password: {
     type: String,
-    required: true,
+    // required: true,
     trim: true,
     minLength: 7,
     validate(value) {
@@ -30,6 +32,7 @@ const User = mongoose.model('User', {
   email: {
     type: String,
     trim: true,
+    unique: true,
     lowercase: true,
     validate(value) {
       if (!validator.isEmail(value)) {
@@ -37,6 +40,67 @@ const User = mongoose.model('User', {
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+// defines function that can be accessed through instance since token will be unique for each user
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, 'thisisatest');
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+// defines a function in userModel so that it can be accessed
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!email) {
+    throw new Error('email/password incorrect');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('email/password incorrect');
+  }
+
+  return user;
+};
+
+// hash the plain password
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+User.createIndexes();
 
 module.exports = User;
